de56b95df6ffa572322096060fa4218718eb0c8e
TalYehuda <Talyehuda27@gmail.com>
2025-03-01 22:40:04 +0200

    Added MassTransit message bus for create,update,delete

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,10 +16,12 @@ public class AuctionsController : ControllerBase
 {
     private readonly AuctionDbContext _context;
     private readonly IMapper _mapper;
-    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    private readonly IPublishEndpoint _publishEndpoint;
+    public AuctionsController(AuctionDbContext context, IMapper mapper,IPublishEndpoint publishEndpoint)
     {
         _context = context;
         _mapper = mapper;
+        _publishEndpoint = publishEndpoint;
     }
 
     [HttpGet]
@@ -31,14 +35,8 @@ public class AuctionsController : ControllerBase
         }
 
         return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
-        //var auctions = await _context.Auctions
-        //    .Include(x => x.Item)
-        //    .OrderBy(x => x.Item.Make)
-        //    .ToListAsync();
-
-        //return _mapper.Map<List<AuctionDto>>(auctions);
     }
-
+    
     [HttpGet("{id}")]
     public async Task<ActionResult<AuctionDto>> GetActionById(Guid id)
     {
@@ -60,11 +58,16 @@ public class AuctionsController : ControllerBase
         auction.Seller = "Test";
         _context.Auctions.Add(auction);
 
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+
+        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+
         var result = await _context.SaveChangesAsync() > 0;
-        if(!result) return BadRequest("Couldnt save changes to the DB");
+
+        if (!result) return BadRequest("Couldnt save changes to the DB");
         
         return CreatedAtAction(nameof(GetActionById),
-            new {auction.Id}, _mapper.Map<AuctionDto>(auction));
+            new {auction.Id}, newAuction);
     }
 
     [HttpPut("{id}")]
@@ -81,6 +84,8 @@ public class AuctionsController : ControllerBase
         auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
         auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
         auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
 
         var result = await _context.SaveChangesAsync() > 0;
 
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,17 +1,36 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
-using SearchService.Models;
 using SearchService.Services;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search",false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+            e.UseMessageRetry(r => r.Interval(5, 5));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+
+});
 
 var app = builder.Build();
 
@@ -38,5 +57,5 @@ app.Run();
 static IAsyncPolicy<HttpResponseMessage> GetPolicy()
     => HttpPolicyExtensions
     .HandleTransientHttpError()
-    .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+    .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
     .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(5));
