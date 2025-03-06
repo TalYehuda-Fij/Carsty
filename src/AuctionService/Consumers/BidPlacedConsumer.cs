9259f1a256ec0f553f42564c63c67ce4dc23b62b
TalYehuda <Talyehuda27@gmail.com>
2025-03-06 12:49:49 +0200

    configured gateway service

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using AuctionService.DataLayer;
+using Contracts;
+using MassTransit;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace AuctionService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbcontext;
+    public BidPlacedConsumer(AuctionDbContext dbcontext)
+    {
+        _dbcontext = dbcontext;
+    }
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("Consuming bid placed");
+
+        var auction = await _dbcontext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction.CurrentHighBid == null || context.Message.BidStatus.Contains("accepted") &&
+            context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await _dbcontext.SaveChangesAsync();
+        }
+    }
+}
